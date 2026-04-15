3dc6416772964351cc3ba75c1bc700ff296e2877
fastball-marty <ecarlson1317@gmail.com>
2026-04-15 17:15:32 -0400

    update

--- src/pages/Starters.tsx
+++ src/pages/Starters.tsx
@@ -14,10 +14,16 @@ const Starters = () => {
     const q2 = parseFloat(item2);
     const p1 = parseFloat(price1);
 
+    if (!q1 || !q2 || !p1 || isNaN(q1) || isNaN(q2) || isNaN(p1)) {
+      alert("Please enter numbers only");
+      return;
+    }
+
     const unitToGrams: Record<string, number> = {
       lb: 453.6,
       gallon: 3785,
       liter: 1000,
+      quart: 946,
       cup: 240,
       oz: 28.35,
       tbsp: 15,
@@ -36,44 +42,52 @@ const Starters = () => {
   }
 
   return (
-    <div id="calculator">
-      <div className="row">
-        <label>Item Quantity: </label>
-        <input value={item1} onChange={(e) => setItem1(e.target.value)} />
-        <select value={metric1} onChange={(e) => setMetric1(e.target.value)}>
-          <option value="lb">lb</option>
-          <option value="gallon">gallon</option>
-          <option value="liter">liter</option>
-          <option value="cup">cup</option>
-          <option value="oz">oz</option>
-          <option value="tbsp">tbsp</option>
-          <option value="tsp">tsp</option>
-        </select>
-        <label>Price: </label>
-        <input
-          value={price1}
-          onChange={(e) => setPrice1(e.target.value)}
-        />{" "}
-      </div>
+    <>
+      <p>
+        <i>Enter an item and its price, and a quantity to convert to.</i>
+      </p>
+      <div id="calculator">
+        <div className="row">
+          <label>Item Quantity: </label>
+          <input value={item1} onChange={(e) => setItem1(e.target.value)} />
+          <select value={metric1} onChange={(e) => setMetric1(e.target.value)}>
+            <option value="lb">lb</option>
+            <option value="gallon">gallon</option>
+            <option value="liter">liter</option>
+            <option value="quart">quart</option>
+            <option value="cup">cup</option>
+            <option value="oz">oz</option>
+            <option value="tbsp">tbsp</option>
+            <option value="tsp">tsp</option>
+          </select>
+          <label>Price: </label>
+          <input value={price1} onChange={(e) => setPrice1(e.target.value)} />
+        </div>
 
-      <div className="row">
-        <label>Item Quantity: </label>
-        <input value={item2} onChange={(e) => setItem2(e.target.value)} />{" "}
-        <select value={metric2} onChange={(e) => setMetric2(e.target.value)}>
-          <option value="lb">lb</option>
-          <option value="gallon">gallon</option>
-          <option value="liter">liter</option>
-          <option value="cup">cup</option>
-          <option value="oz">oz</option>
-          <option value="tbsp">tbsp</option>
-          <option value="tsp">tsp</option>
-        </select>
-        <label>Price: </label>
-        <input value={price2} disabled={true} />{" "}
-      </div>
+        <div className="row">
+          <label>Item Quantity: </label>
+          <input value={item2} onChange={(e) => setItem2(e.target.value)} />
+          <select value={metric2} onChange={(e) => setMetric2(e.target.value)}>
+            <option value="lb">lb</option>
+            <option value="gallon">gallon</option>
+            <option value="liter">liter</option>
+            <option value="quart">quart</option>
+            <option value="cup">cup</option>
+            <option value="oz">oz</option>
+            <option value="tbsp">tbsp</option>
+            <option value="tsp">tsp</option>
+          </select>
+          <label>Price: </label>
+          <input value={price2} disabled={true} />{" "}
+        </div>
 
-      <button onClick={convert}>Go</button>
-    </div>
+        <button onClick={convert}>Go</button>
+
+        <p className="footer">
+          <i>I love you!</i>
+        </p>
+      </div>
+    </>
   );
 };
 
