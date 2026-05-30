bc7e2df8fceda93931df1c5dbf60c88550873dad
fastball-marty <ecarlson1317@gmail.com>
2026-05-30 19:44:54 -0400

    updates

--- src/pages/Starters.tsx
+++ src/pages/Starters.tsx
@@ -1,92 +1,116 @@
 import React, { useState } from "react";
 import "../styles/styles.css";
+import { PriceConversionItem } from "../types/types";
 
 const Starters = () => {
-  const [item1, setItem1] = useState("");
-  const [item2, setItem2] = useState("");
-  const [metric1, setMetric1] = useState("lb");
-  const [metric2, setMetric2] = useState("tsp");
-  const [price1, setPrice1] = useState("");
-  const [price2, setPrice2] = useState("");
+  const [item1, setItem1] = useState<PriceConversionItem>({
+    quantity: "10",
+    unit: "lb",
+    price: "10",
+  });
+
+  const [item2, setItem2] = useState<PriceConversionItem>({
+    quantity: "1",
+    unit: "tsp",
+    price: "0.01",
+  });
+
+  const unitToGrams: Record<string, number> = {
+    lb: 453.6,
+    gallon: 3785,
+    liter: 1000,
+    quart: 946,
+    cup: 240,
+    oz: 28.35,
+    tbsp: 15,
+    tsp: 5,
+    g: 1,
+  };
+
+  const renderUnitOptions = () => {
+    return Object.entries(unitToGrams).map(([unit]) => (
+      <option key={unit} value={unit}>
+        {unit}
+      </option>
+    ));
+  };
+
+  function handleUpdateItem1(key: string, value: string) {
+    setItem1((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  }
+
+  function handleUpdateItem2(key: string, value: string) {
+    setItem2((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  }
 
   function convert() {
-    const q1 = parseFloat(item1);
-    const q2 = parseFloat(item2);
-    const p1 = parseFloat(price1);
+    const g1 = parseFloat(item1.quantity) * unitToGrams[item1.unit];
+    const g2 = parseFloat(item2.quantity) * unitToGrams[item2.unit];
 
-    if (!q1 || !q2 || !p1 || isNaN(q1) || isNaN(q2) || isNaN(p1)) {
-      alert("Please enter numbers only");
-      return;
-    }
+    const pricePerGram = parseFloat(item1.price) / g1;
+    const p2 = pricePerGram * g2;
 
-    const unitToGrams: Record<string, number> = {
-      lb: 453.6,
-      gallon: 3785,
-      liter: 1000,
-      quart: 946,
-      cup: 240,
-      oz: 28.35,
-      tbsp: 15,
-      tsp: 5,
-    };
-
-    // convert both qtys to grams
-    const grams1 = q1 * unitToGrams[metric1];
-    const grams2 = q2 * unitToGrams[metric2];
-
-    // calculate p2
-    const pricePerGram = p1 / grams1;
-    const p2 = pricePerGram * grams2;
-
-    setPrice2(`$${p2.toFixed(2)}`);
+    handleUpdateItem2("price", p2.toFixed(2));
   }
 
   return (
     <>
       <p>
-        <i>Enter an item and its price, and a quantity to convert to.</i>
+        <i>
+          Enter an item and its price, and a target quantity to calculate its
+          cost.
+        </i>
       </p>
+
       <div id="calculator">
         <div className="row">
-          <label>Item Quantity: </label>
-          <input value={item1} onChange={(e) => setItem1(e.target.value)} />
-          <select value={metric1} onChange={(e) => setMetric1(e.target.value)}>
-            <option value="lb">lb</option>
-            <option value="gallon">gallon</option>
-            <option value="liter">liter</option>
-            <option value="quart">quart</option>
-            <option value="cup">cup</option>
-            <option value="oz">oz</option>
-            <option value="tbsp">tbsp</option>
-            <option value="tsp">tsp</option>
+          <label>Quantity: </label>
+          <input
+            value={item1.quantity}
+            onChange={(e) => handleUpdateItem1("quantity", e.target.value)}
+          />
+          <select
+            value={item1.unit}
+            onChange={(e) => handleUpdateItem1("unit", e.target.value)}
+          >
+            {renderUnitOptions()}
           </select>
-          <label>Price: </label>
-          <input value={price1} onChange={(e) => setPrice1(e.target.value)} />
+
+          <label>Price: $</label>
+          <input
+            value={item1.price}
+            onChange={(e) => handleUpdateItem1("price", e.target.value)}
+          />
         </div>
 
         <div className="row">
-          <label>Item Quantity: </label>
-          <input value={item2} onChange={(e) => setItem2(e.target.value)} />
-          <select value={metric2} onChange={(e) => setMetric2(e.target.value)}>
-            <option value="lb">lb</option>
-            <option value="gallon">gallon</option>
-            <option value="liter">liter</option>
-            <option value="quart">quart</option>
-            <option value="cup">cup</option>
-            <option value="oz">oz</option>
-            <option value="tbsp">tbsp</option>
-            <option value="tsp">tsp</option>
+          <label>Quantity: </label>
+          <input
+            value={item2.quantity}
+            onChange={(e) => handleUpdateItem2("quantity", e.target.value)}
+          />
+          <select
+            value={item2.unit}
+            onChange={(e) => handleUpdateItem2("unit", e.target.value)}
+          >
+            {renderUnitOptions()}
           </select>
-          <label>Price: </label>
-          <input value={price2} disabled={true} />{" "}
+          <label>Price: $</label>
+          <input value={item2.price} disabled />
         </div>
 
-        <button onClick={convert}>Go</button>
-
-        <p className="footer">
-          <i>I love you!</i>
-        </p>
+        <button onClick={convert}>Calculate</button>
       </div>
+
+      <p className="footer">
+        <i>I love you!</i>
+      </p>
     </>
   );
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,9 @@ export type ButtonProps = {
   children: string;
   onClick?: () => void;
 };
+
+export type PriceConversionItem = {
+  quantity: string;
+  unit: string;
+  price: string;
+};
