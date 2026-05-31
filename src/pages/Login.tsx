063386909de7ac425730404bb6069af3a4bc1ade
fastball-marty <ecarlson1317@gmail.com>
2026-05-30 22:16:56 -0400

    create login page, deploy

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,39 @@
+import React, { useState } from 'react';
+import "../styles/styles.css";
+ 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+ 
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log('Username:', username, 'Password:', password);
+    };
+ 
+    return (
+      <>
+        <form id="login-box" onSubmit={handleSubmit}>
+          <input
+            className="login-row"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="login-row"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Sign in</button>
 
-  return (
-    <p>Login</p>
-  )
-}
-
+          <i className="register-text" onClick={() => console.log('register')}><u>New here? Click to register.</u></i>
+        </form>
+      </>
+      
+    );
+};
+ 
 export default Login;
